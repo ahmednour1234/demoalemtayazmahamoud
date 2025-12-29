2fa34bb33b4b81f1a3ced4da864caeafb92295f1
ahmednour1234 <ahmednour5999@gmail.com>
2025-12-30 00:57:34 +0200

    www

---
+++ database/seeders/Table015Seeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Table015Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('table_015')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+}
